c0c0e43c23f7c3f33210c5c19ee9fe8a987ccaf6
srand2 <62186203+srand2@users.noreply.github.com>
2023-05-18 16:13:06 +0300

    Update ShadowScan.ps1

--- ShadowScan.ps1
+++ ShadowScan.ps1
@@ -1,4 +1,4 @@
-[CmdletBinding(SupportsShouldProcess=$true)]
+[CmdletBinding()]
 param (
     [Parameter(Mandatory=$false, HelpMessage="Specify the path to the IP file. If not provided, it will look for ips.txt in the local folder.")]
     [string]$ipFile = "ips.txt",
@@ -7,7 +7,6 @@ param (
     [int[]]$customPorts,
 
     [switch]$help
-
 )
 
 function Show-Help {
@@ -40,85 +39,85 @@ EXAMPLES
     Write-Host $helpText
 }
 
-    if ($ipFile -and (-not (Test-Path $ipFile -PathType Leaf))) {
-        $ipFile = Join-Path $PSScriptRoot $ipFile
-    }
-
 if ($help) {
     Show-Help
     exit
 }
 
-    $ipAddresses = Get-Content $ipFile
+if ($ipFile -and (-not (Test-Path $ipFile -PathType Leaf))) {
+    $ipFile = Join-Path $PSScriptRoot $ipFile
+}
 
-    $aliveHosts = @()
+$ipAddresses = Get-Content $ipFile
 
-    Write-Host "Pinging hosts to check for alive hosts..."
+$aliveHosts = @()
 
-    foreach ($ip in $ipAddresses) {
-        $result = Test-Connection -ComputerName $ip -Count 1 -Quiet
-        if ($result) {
-            $aliveHosts += $ip
-            Write-Host "Host $ip is alive." -ForegroundColor Green
-        } else {
-            Write-Host "Host $ip is not responding." -ForegroundColor Red
-        }
+Write-Host "Pinging hosts to check for alive hosts..."
+
+foreach ($ip in $ipAddresses) {
+    $result = Test-Connection -ComputerName $ip -Count 1 -Quiet
+    if ($result) {
+        $aliveHosts += $ip
+        Write-Host "Host $ip is alive." -ForegroundColor Green
+    } else {
+        Write-Host "Host $ip is not responding." -ForegroundColor Red
     }
+}
 
-    if ($aliveHosts.Count -eq 0) {
-        Write-Host "No hosts are alive. Exiting..." -ForegroundColor Yellow
-        exit
+if ($aliveHosts.Count -eq 0) {
+    Write-Host "No hosts are alive. Exiting..." -ForegroundColor Yellow
+    exit
+}
+
+$defaultPorts = @(80, 443, 445, 7070, 7071, 4786, 4848, 5555, 5556, 3300, 6129, 6379, 6970)
+$portServices = @{
+    80    = "HTTP";
+    443   = "HTTPS";
+    445   = "SMB";
+    7070  = "WebLogic";
+    7071  = "WebLogic";
+    4786  = "Cisco Smart Install";
+    4848  = "GlassFish";
+    5555  = "HP Data Protector";
+    5556  = "HP Data Protector";
+    3300  = "SAP";
+    6129  = "DameWare";
+    6379  = "Redis";
+    6970  = "Cisco Unified Comm Manager"
+}
+
+Write-Host "Scanning open ports on alive hosts..."
+
+foreach ($aliveHost in $aliveHosts) {
+    $ip = $aliveHost
+    $firstThreeOctets = $ip -split '\.' | Select-Object -First 3
+    $joined = $firstThreeOctets -join '.'
+    $a = $ip -split '\.' | Select-Object -Last 1
+    $combinedIP = "$joined.$a"
+    Write-Host "Scanning ports for host $ip..."
+    $portsToScan = $defaultPorts
+    if ($customPorts) {
+        $portsToScan = $customPorts
     }
-
-    $defaultPorts = @(80, 443, 445, 7070, 7071, 4786, 4848, 5555, 5556, 3300, 6129, 6379, 6970)
-    $portServices = @{
-        80    = "HTTP";
-        443   = "HTTPS";
-        445   = "SMB";
-        7070  = "WebLogic";
-        7071  = "WebLogic";
-        4786  = "Cisco Smart Install";
-        4848  = "GlassFish";
-        5555  = "HP Data Protector";
-        5556  = "HP Data Protector";
-        3300  = "SAP";
-        6129  = "DameWare";
-        6379  = "Redis";
-        6970  = "Cisco Unified Comm Manager"
-    }
-
-    Write-Host "Scanning open ports on alive hosts..."
-
-    foreach ($aliveHost in $aliveHosts) {
-        $ip = $aliveHost
-        $firstThreeOctets = $ip -split '\.' | Select-Object -First 3
-        $joined = $firstThreeOctets -join '.'
-        $a = $ip -split '\.' | Select-Object -Last 1
-        $combinedIP = "$joined.$a"
-        Write-Host "Scanning ports for host $ip..."
-        $portsToScan = $defaultPorts
-        if ($customPorts) {
-            $portsToScan = $customPorts
-        }
-        foreach ($port in $portsToScan) {
-            try {
-                $tcpClient = New-Object Net.Sockets.TcpClient
-                $asyncResult = $tcpClient.BeginConnect($combinedIP, $port, $null, $null)
-                $waitHandle = $asyncResult.AsyncWaitHandle
-                $result = $waitHandle.WaitOne(1000, $false)
-                if ($result -and $tcpClient.Connected) {
-                    Write-Host "Port $port is open on $ip! Service: $($portServices[$port])" -ForegroundColor Green
-                    $tcpClient.EndConnect($asyncResult) | Out-Null
-                } else {
-                    Write-Host "Port $port is closed on $ip." -ForegroundColor Red
-                }
-                $waitHandle.Dispose()
-                $tcpClient.Close()
-                Start-Sleep -Milliseconds (Get-Random -Minimum 500 -Maximum 1000)
-            } catch {
-                $errorMessage = $_.Exception.Message
-                Write-Host $errorMessage -ForegroundColor Yellow
+    foreach ($port in $portsToScan) {
+        try {
+            $tcpClient = New-Object Net.Sockets.TcpClient
+            $asyncResult = $tcpClient.BeginConnect($combinedIP, $port, $null, $null)
+            $waitHandle = $asyncResult.AsyncWaitHandle
+            $result = $waitHandle.WaitOne(1000, $false)
+            if ($result -and $tcpClient.Connected) {
+                Write-Host "Port $port is open on $ip! Service: $($portServices[$port])" -ForegroundColor Green
+                $tcpClient.EndConnect($asyncResult) | Out-Null
+            } else {
+                Write-Host "Port $port is closed on $ip." -ForegroundColor Red
             }
-            Start-Sleep -Milliseconds (Get-Random -Minimum 200 -Maximum 500)
+            $waitHandle.Dispose()
+            $tcpClient.Close()
+            Start-Sleep -Milliseconds (Get-Random -Minimum 500 -Maximum 1000)
+        } catch {
+            $errorMessage = $_.Exception.Message
+            Write-Host $errorMessage -ForegroundColor Yellow
         }
-
+        Start-Sleep -Milliseconds (Get-Random -Minimum 200 -Maximum 500)
+    }
+}
